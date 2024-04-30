0bd265cd3ac48ce6450804bd9e286f53e8ffc67b
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-04-30 21:42:12 +0100

    Update SaveTo.java

--- src/SaveTo.java
+++ src/SaveTo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public @interface SaveTo {
-}
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SaveTo {
+        String path();
+    }
+
+    @Target(value = ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+@interface Save{
+
+    }
