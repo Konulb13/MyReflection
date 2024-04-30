59f804f263c9695e55754a7de280083702eb92d2
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-04-30 21:42:38 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+       TextContainer tc = new TextContainer();
+       Class<?> c = TextContainer.class;
+        Method []methods = c.getDeclaredMethods();
+        if(c.isAnnotationPresent(SaveTo.class)){
+            SaveTo st = c.getAnnotation(SaveTo.class);
+            for (Method method: methods){
+                if(method.isAnnotationPresent(SaveTo.class)){
+                    method.invoke(tc,st.path());
+                }
+            }
+        }
     }
 }
