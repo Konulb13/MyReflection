112b90656807421afcfe1e7a46193f70d8ef8bc3
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-04-30 21:36:33 +0100

    Update TextContainer.java

--- src/TextContainer.java
+++ src/TextContainer.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class TextContainer {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+@SaveTo(
+    path = " text.txt "
+)
+public class TextContainer {
+    private String text;
+
+    public TextContainer() {
+        this.text = this.text;
+    }
+
+    @Save
+    public void save(String path) {
+        try {
+            PrintWriter printWriter = new PrintWriter(path);
+
+            try {
+                printWriter.println(this.text);
+            } catch (Throwable var6) {
+                try {
+                    printWriter.close();
+                } catch (Throwable var5) {
+                    var6.addSuppressed(var5);
+                }
+
+                throw var6;
+            }
+
+            printWriter.close();
+        } catch (FileNotFoundException var7) {
+            FileNotFoundException e = var7;
+            e.printStackTrace();
+        }
+
+    }
 }
