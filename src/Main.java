f3cef56e4be363788ee78d564707cc608a38d674
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-05-02 21:42:05 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,16 @@ import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        MyAnnClass an = new MyAnnClass();
-        Class<?> cl = an.getClass();
-        Method[] allMethods = cl.getDeclaredMethods();
-        for (Method m : allMethods) {
-            if (m.isAnnotationPresent(MyAnnotation.class)) {
-                MyAnnotation myAnnotation = m.getAnnotation(MyAnnotation.class);
-                System.out.println(m.invoke(an, myAnnotation.a(), myAnnotation.b()));
+       TextContainer tc = new TextContainer();
+       Class<?> c = TextContainer.class;
+        Method []methods = c.getDeclaredMethods();
+        if(c.isAnnotationPresent(SaveTo.class)){
+            SaveTo st = c.getAnnotation(SaveTo.class);
+            for (Method method: methods){
+                if(method.isAnnotationPresent(SaveTo.class)){
+                    method.invoke(tc,st.path());
+                }
             }
-
         }
-    }
-}
-
-class MyAnnClass {
-    @MyAnnotation(a = 42, b = 38)
-    public int annMethod(int a, int b) {
-        System.out.println("a = " + a + "\n" + "b = " + b);
-        return a+b;
     }
 }
